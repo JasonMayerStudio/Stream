9ea1a4e07495edc79c5ff903d17d9d5d0750cbec
Jason Mayer <jason@jasonmayer.com>
2017-06-25 12:22:17 -0400

    components

---
+++ Pipes.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Pipes= styled.div`
+    float:left;
+	width:406px;
+	height:364px;
+	background-color:#FFFFFF;
+
+`;
+
+
+
+
+
+
+export default Pipes;
---
+++ usage.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const usage= styled.div`
+    padding-top:20px;
+	width:1109px;
+	height:400px;
+	background-color:#FFFFFF;
+
+`;
+
+
+
+
+
+
+export default usage;
